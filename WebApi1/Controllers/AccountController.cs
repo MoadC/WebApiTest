dbc28c335824c8d291449eff0443548e717cec09
MoadC <rappelzkloyster@gmail.com>
2022-06-12 20:21:02 +0100

    Dto Added

--- WebApi1/Controllers/AccountController.cs
+++ WebApi1/Controllers/AccountController.cs
@@ -1,6 +1,45 @@
-﻿namespace WebApi1.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using WebApi1.Data;
+using WebApi1.DTOs;
+using WebApi1.Entities;
+
+namespace WebApi1.Controllers
 {
-    public class Class
+    public class AccountController : BaseApiController
     {
+        private DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("UserName is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
     }
 }
---
+++ WebApi1/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi1.DTOs
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
